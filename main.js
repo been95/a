4ab3ea6a7741676d80709d8751c65aca97fba4c8
yeomcheabeen <dcb95@naver.com>
2021-01-16 18:13:32 +0900

    메뉴추가

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("hollow");
+"use strict";
+
+const button = document.querySelector("#opener");
+const main = document.querySelector("#main");
+
+button.addEventListener("click", function () {
+  console.log("버튼클릭햇음");
+  main.style.transform = "translateX(0px)";
+});
